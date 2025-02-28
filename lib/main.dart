fbc142dba6d48d649858c5a515eed76693bda725
Christian Koscielniak Pinto <71388250+ChrisKp1710@users.noreply.github.com>
2025-02-28 17:47:49 +0100

    Aggiungi supporto per Firebase Analytics e configura le opzioni Firebase per diverse piattaforme

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,27 @@
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_application_1/auth_screen.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+
+final FirebaseAnalytics analytics = FirebaseAnalytics.instance;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-
-  runApp(MyApp());
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: AuthScreen(), // Schermata di autenticazione iniziale
+      home: AuthScreen(),
+      // Aggiungi il NavigatorObserver per tracciare i passaggi di schermata
+      navigatorObservers: [FirebaseAnalyticsObserver(analytics: analytics)],
     );
   }
 }
